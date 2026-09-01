82eff156665d759347505fb220482058c03ba909
jiwoo1012 <jiwoo6306@gmail.com>
2026-09-01 17:31:02 +0900

    feat: AI 추천 기능 및 결과 페이지 개선

---
+++ src/pages/AiCurator/AiPreference.jsx
@@ -0,0 +1,12 @@
+// Page placeholder.
+import PagePlaceholder from '../../components/common/PagePlaceholder'
+import styles from './AiPreference.module.scss'
+
+const AiPreference = () => (
+  <PagePlaceholder
+    title="내 취향 분석"
+    styles={styles}
+  />
+)
+
+export default AiPreference
